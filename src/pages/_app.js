9d1a48644bfefddfa7715215f25e90f90ddf88ff
rajeshn95 <rajesh@betalectic.com>
2023-06-08 18:50:12 +0530

    comit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ThemeProvider, theme } from "@reusejs/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider value={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "test-demo": "#243c5a",
+      },
+    },
+  },
+  plugins: [],
+};
